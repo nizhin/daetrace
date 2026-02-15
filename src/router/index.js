79ef06b0620319fe60826b4ff7bbf700f48538d7
Vy Bui Nguyen <buing004@umn.edu>
2026-02-14 20:45:22 -0600

    set up router links

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,54 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { getCurrentUser, useFirebaseAuth } from 'vuefire'
+import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth'
+
+import Dashboard from '../views/Dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/category',
+      name: 'category',
+      component: () => import('../views/Category.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/session',
+      name: 'session',
+      component: () => import('../views/Session.vue'),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: '/404',
+      name: 'not-found',
+      component: () => import('../views/NotFoundView.vue'),
+    }
+  ],
+})
+
+async function requireAuth() {
+  const user = await getCurrentUser()
+  if (user) {
+    return true
+  }
+
+  const auth = useFirebaseAuth()
+  const provider = new GoogleAuthProvider
+  await signInWithRedirect(auth, provider)
+  return false
+}
+
+router.beforeEach(async (to) => {
+  if (to.meta.requiresAuth) {
+    return await requireAuth()
+  }
 })
 
 export default router
