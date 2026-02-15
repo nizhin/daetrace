916c4fbc62624321b41214319d0d7d88aafd2d1c
Annalise Xiao <annalisexiao@Annalises-MacBook-Air-4.local>
2026-02-14 23:27:29 -0600

    updated topsiteslist, categorylist

---
+++ src/components/TopSitesList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card">
+        <DataTable :value="products" tableStyle="min-width: 50rem">
+            <Column field="domain" header="Domain"></Column>
+            <Column field="timeSpent" header="Time Spent"></Column>
+            <Column field="Category" header="Category"></Column>
+            <Column field="percent" header="% of Total"></Column>
+        </DataTable>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const sessions = ref([
+    {
+        domain: "youtube.com",
+        timeSpent: "1h 12m",
+        category: "Not Productive",
+        percent: "25%"
+    }
+]);
+
+</script>
