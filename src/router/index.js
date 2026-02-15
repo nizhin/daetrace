7e810de727c1b3abf54142effc5b94d606921d30
Vy Bui Nguyen <buing004@umn.edu>
2026-02-14 22:47:42 -0600

    set up router links and log in page

--- src/router/index.js
+++ src/router/index.js
@@ -1,49 +1,55 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { getCurrentUser, useFirebaseAuth } from 'vuefire'
-import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth'
+import { createRouter, createWebHistory } from "vue-router";
+import { getCurrentUser, useFirebaseAuth } from "vuefire";
+import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 
-import Dashboard from '../views/Dashboard.vue'
+import Dashboard from "../views/Dashboard.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/dashboard',
-      name: 'dashboard',
+      path: "/login",
+      name: "login",
+      component: () => import("../views/LogIn.vue"),
+      meta: { loggedIn: true },
+    },
+    {
+      path: "/",
+      name: "dashboard",
       component: Dashboard,
       meta: { requiresAuth: true },
     },
     {
-      path: '/category',
-      name: 'category',
-      component: () => import('../views/Category.vue'),
+      path: "/category",
+      name: "category",
+      component: () => import("../views/Category.vue"),
       meta: { requiresAuth: true },
     },
     {
-      path: '/session',
-      name: 'session',
-      component: () => import('../views/Session.vue'),
+      path: "/session",
+      name: "session",
+      component: () => import("../views/Session.vue"),
       meta: { requiresAuth: true },
-    }
+    },
   ],
-})
+});
 
 async function requireAuth() {
-  const user = await getCurrentUser()
+  const user = await getCurrentUser();
   if (user) {
-    return true
+    return true;
   }
 
-  const auth = useFirebaseAuth()
-  const provider = new GoogleAuthProvider
-  await signInWithRedirect(auth, provider)
-  return false
+  const auth = useFirebaseAuth();
+  const provider = new GoogleAuthProvider();
+  await signInWithRedirect(auth, provider);
+  return false;
 }
 
 router.beforeEach(async (to) => {
   if (to.meta.requiresAuth) {
-    return await requireAuth()
+    return await requireAuth();
   }
-})
+});
 
-export default router
+export default router;
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,3 +1,8 @@
 <template>
-  <div>Dashboard</div>
+  <div class="p-4">
+    <h1>Dashboard</h1>
+    <p class="text-green-600 font-bold">page is working.</p>
+  </div>
 </template>
+
+<script setup></script>
