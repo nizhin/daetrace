4f83471d38e19cc7b0fcde24f6a14b0f8e9a4742
Annalise Xiao <annalisexiao@Annalises-MacBook-Air-4.local>
2026-02-14 22:54:24 -0600

    daily overview, prodscore update

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,29 @@
 <template>
     <div class="card">
-        <Menubar :model="items" />
+        <Menubar :model="items">
+            <template #end>
+                <Button
+                    label="Log Out"
+                    icon="pi pi-sign-out"
+                    @click="logout"
+                />
+            </template>
+        </Menubar>
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import Menubar from 'primevue/menubar'
+import Button from 'primevue/button'
 
 const items = ref([
     { label: 'Dashboard', icon: 'pi pi-home', route: '/'}, 
     { label: 'Session', icon: 'pi pi-clock', route: '/session'}, 
     { label: 'Category', icon: 'pi pi-list', route: '/category'},
 ])
+
+const logout = () => {
+    console.log("Logging out...")
+}
 </script>
