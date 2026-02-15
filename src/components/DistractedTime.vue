805a1629e97a90fac373a076d7e53490415a5cb0
Annalise Xiao <annalisexiao@infra04-wg312.eduroam.wireless.umn.edu>
2026-02-14 20:48:54 -0600

    focustime, distractedtime, navbar

---
+++ src/components/DistractedTime.vue
@@ -0,0 +1,18 @@
+<template>
+    <Card>
+        <template #title>
+            <div class="flex items-center gap-2">
+                <i class="pi pi-eye-slash text-red-500 text-lg"></i>
+                <span>Distracted Time</span>
+            </div>
+        </template>
+        <template #content>
+            <p class="m-0">
+                3h 9m
+            </p>
+        </template>
+    </Card>
+</template>
+
+<script setup>
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card">
+        <Menubar :model="items" />
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const items = ref([
+    { label: 'Dashboard', icon: 'pi pi-home', route: '/'}, 
+    { label: 'Session', icon: 'pi pi-clock', route: '/session'}, 
+    { label: 'Category', icon: 'pi pi-list', route: '/category'},
+])
+</script>
